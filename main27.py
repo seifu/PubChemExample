a44d7f4b2b76e33160a63260b5d386ec59829fc3
seifu <chondesj@gmail.com>
2014-09-22 13:53:34 -0400

    added 2.7 compatible file

--- main27.py
+++ main27.py
@@ -1,11 +1,11 @@
 import re
 import os
-from BeautifulSoup import BeautifulStoneSoup
+from bs4 import BeautifulSoup
 
 __author__ = 'sjc294@psu.edu'
 
 
-def get_files(mypath="C:\Users\ToastyDelight\Desktop\AB"):
+def get_files(mypath="C:\\Users\\ToastyDelight\\Desktop\\AB"):
     #This gets the file names
     f = []
     for (dirpath, _, filenames) in os.walk(mypath):
@@ -15,34 +15,43 @@ def get_files(mypath="C:\Users\ToastyDelight\Desktop\AB"):
 
 def read_file(filename):
     #This reads the files and makes the soup
-    print filename
+    print(filename)
     f = open(filename, 'r')
-    readfile = BeautifulStoneSoup(f)
+    readfile = BeautifulSoup(f)
     f.close()
     return readfile
 
 
 if __name__ == "__main__":
-    #linux file path
+
+    #home file path (linux)
     #"/media/toastydelight/5C38A5EB38A5C3FC/Data/Calcif_Tissue_Int/Calcif_Tissue_Int_2007_Dec_4_81(6)_421-429.nxml"
-    #thumbdrive file path
+
+    #thumbdrive file path (windows)
     #"F:\Research\Cheminformatics\Data\PMC\AB"
 
-    filelist = get_files("/media/toastydelight/5C38A5EB38A5C3FC/Data/Documents/Research/Cheminformatics/Data/CH")
+    #work file path (windows)
+    #"C:\SeifusFiles\Research\Cheminformatics\Data\AB\AAPS_J"
+
+    #last used path
+    #"/media/toastydelight/5C38A5EB38A5C3FC/Data/Documents/Research/Cheminformatics/Data/CH"
+
+    filelist = get_files("C:\\SeifusFiles\\Research\\Cheminformatics\\Data\\AB\\Anticancer_Agents_Med_Chem")
     #print len(filelist)
     #print "hello"
     for t in filelist:
         soup = read_file(t)
         try:
-            title = re.sub("\n", "", soup.find(["article-title"]).renderContents())
+            title = re.sub("\n", "", soup.find(["article-title"]).get_text())
             title = re.sub(",", "", title)
-            year = soup.find(["year"]).renderContents()
+            year = soup.find(["year"]).get_text()
             kwd = soup.findAll(["kwd"])
             if len(kwd) > 0:
                 for akwd in kwd:
-                    seq = (t, title, year, re.sub(",", "", akwd.renderContents()))
+                    seq = (t, title, year, re.sub(",", "", akwd.get_text()))
                     out_text = ", ".join(seq)
-                    with open("PMCKwdCH.txt", "ab") as myoutput:
-                        myoutput.write(out_text+"\n")
+                    print(out_text)
+                    #with open("PMCKwdCH.txt", "ab") as myoutput:
+                        #myoutput.write(out_text+"\n")
         finally:
             soup.decompose()
